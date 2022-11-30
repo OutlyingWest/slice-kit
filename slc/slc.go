4bc5c7308a8601b3c5450b1da378dafd122ff1ce
OutlyingWest <alexeybuv7@gmail.com>
2022-11-30 14:44:32 +0300

    corrections in compare func

--- slc/slc.go
+++ slc/slc.go
@@ -63,15 +63,8 @@ func ReverseSlice[T sliceTypes](s []T) []T {
 }
 
 // Comparison of two slices:
-func CompareSlices() {
-	s1 := []int{1, 2, 3}
-	s2 := []int{1, 2, 4}
-	s3 := []string{"1", "2", "3"}
-	s4 := []int{1, 2, 3}
-
-	fmt.Println(reflect.DeepEqual(s1, s2)) // false
-	fmt.Println(reflect.DeepEqual(s1, s3)) // false
-	fmt.Println(reflect.DeepEqual(s1, s4)) // true
+func CompareSlices[T sliceTypes](s1 []T, s2 []T) bool {
+	return reflect.DeepEqual(s1, s2)
 }
 
 // Delete duplications in slice
